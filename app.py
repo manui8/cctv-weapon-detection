b45bcb76e548d2f08ead3d271f545d37b1901649
manui8 <bhavanisatyasri@gmail.com>
2026-08-18 14:06:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -231,13 +231,12 @@ if uploaded_image is not None:
 
         if os.path.exists("alert.mp3"):
             with open("alert.mp3", "rb") as audio_file:
-            audio_bytes = audio_file.read()
-
-            st.audio(
-                audio_bytes,
-                format="audio/mp3",
-                autoplay=True
-            )
+                audio_bytes = audio_file.read()
+                st.audio(
+                    audio_bytes,
+                    format="audio/mp3",
+                    autoplay=True
+                )
 
         else:
 
