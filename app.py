15e5615dcbd91f0b5d93e27ba32cccd57b9c13d6
manui8 <bhavanisatyasri@gmail.com>
2026-08-18 12:46:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -229,6 +229,17 @@ if uploaded_image is not None:
                 f"Confidence: {detected_confidence:.2f}"
             )
 
+        if os.path.exists("alert.mp3"):
+
+    with open("alert.mp3", "rb") as audio_file:
+        audio_bytes = audio_file.read()
+
+    st.audio(
+        audio_bytes,
+        format="audio/mp3",
+        autoplay=True
+    )
+
         else:
 
             st.success(
